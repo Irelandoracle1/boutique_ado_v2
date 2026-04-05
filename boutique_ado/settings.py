16e37dc3cb9ae6fba2c7a93d737fa64c36d25fd2
Oluwaseyi <nairajnr1@gmail.com>
2026-04-05 08:58:48 +0100

    adding discount for shopping bag

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -150,3 +151,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'STATIC'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+FREE_DELIVERY_THRESHOLD = 250
+STANDARD_DELIVERY_PERCENTAGE = 10
+
