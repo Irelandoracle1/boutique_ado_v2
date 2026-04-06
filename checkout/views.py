8cced1035cb86c84e556f3812c6fc663640af7a9
Oluwaseyi <nairajnr1@gmail.com>
2026-04-06 18:47:22 +0100

    added stripe payment logic and styling

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51TDi3yRsGUaViriPtK6XkKqXzgfESE2w1li1M9Q7lpWI9FHrhrilI38SdEI2WSJo1PduenqkQf7ei7RmDyqFFpzX00oVLWzg4y',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
