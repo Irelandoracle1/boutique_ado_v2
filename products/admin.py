deed08151609ab9c43a615c6898404c3bf2da6e6
Oluwaseyi <nairajnr1@gmail.com>
2026-04-06 03:50:28 +0100

    finished product size logic

--- products/admin.py
+++ products/admin.py
@@ -1,25 +1,17 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, ProductSize
 
-# Register your models here.
+class ProductSizeInline(admin.TabularInline):
+    model = ProductSize
+    extra = 1
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = (
-        'sku',
-        'name',
-        'category',
-        'price',
-        'rating',
-        'image',
-    )
-
-    ordering = ('name',)
+    list_display = ('sku', 'name', 'category', 'price', 'rating', 'image')
+    inlines = [ProductSizeInline]  # allows adding sizes in the same form
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = (
-        'friendly_name',
-        'name',
-    )
+    list_display = ('friendly_name', 'name')
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(ProductSize)
