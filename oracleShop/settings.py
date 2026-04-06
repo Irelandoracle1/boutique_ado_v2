4e89827ffc9ae9459af0e7cd681fbbcaa9205124
Oluwaseyi <nairajnr1@gmail.com>
2026-04-06 06:37:54 +0100

    completed shopping bag remove and update buttons

--- oracleShop/settings.py
+++ oracleShop/settings.py
@@ -147,11 +147,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'STATIC'),)
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'STATIC'),)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),  # lowercase
+)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 FREE_DELIVERY_THRESHOLD = 250
 STANDARD_DELIVERY_PERCENTAGE = 10
