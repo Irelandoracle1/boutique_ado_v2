92c49c28df1cc2cdda734e8ad66b25155b86d820
Oluwaseyi <nairajnr1@gmail.com>
2026-04-11 15:42:25 +0100

    product admin- added product view, url and template

--- oracleShop/urls.py
+++ oracleShop/urls.py
@@ -1,4 +1,4 @@
-"""Oracle_shop URL Configuration
+"""boutique_ado URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
