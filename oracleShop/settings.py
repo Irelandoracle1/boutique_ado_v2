476163956f505d69c380327c6886a47476496acd
Oluwaseyi <nairajnr1@gmail.com>
2026-04-13 20:05:03 +0100

    fixing aws s3

--- oracleShop/settings.py
+++ oracleShop/settings.py
@@ -74,10 +74,6 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-# ---------------------------------------------------
-# URLS
-# ---------------------------------------------------
-
 ROOT_URLCONF = "oracleShop.urls"
 WSGI_APPLICATION = "oracleShop.wsgi.application"
 
@@ -109,9 +105,11 @@ TEMPLATES = [
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+        default=os.getenv(
+            "DATABASE_URL",
+            f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+        ),
         conn_max_age=600,
-        ssl_require=False,
     )
 }
 
@@ -136,7 +134,7 @@ USE_I18N = True
 USE_TZ = True
 
 # ---------------------------------------------------
-# STATIC FILES (WHITENOISE)
+# STATIC FILES (WHITE NOISE)
 # ---------------------------------------------------
 
 STATIC_URL = "/static/"
@@ -145,22 +143,24 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # ---------------------------------------------------
-# AWS S3 MEDIA STORAGE (ONLY STORAGE USED)
+# AWS S3 MEDIA STORAGE (FINAL FIX)
 # ---------------------------------------------------
 
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", "us-east-1")
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", "eu-north-1")
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
-AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+# IMPORTANT: correct endpoint for eu-north-1
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.eu-north-1.amazonaws.com"
 
-AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
+AWS_S3_FILE_OVERWRITE = False
 AWS_QUERYSTRING_AUTH = False
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
+# IMPORTANT FIX (correct media path)
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
 
 # ---------------------------------------------------
@@ -176,7 +176,7 @@ LOGIN_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/"
 
 # ---------------------------------------------------
-# DJANGO-ALLAUTH (CLEAN VERSION)
+# DJANGO ALLAUTH
 # ---------------------------------------------------
 
 ACCOUNT_LOGIN_METHODS = {"username", "email"}
@@ -192,7 +192,7 @@ ACCOUNT_EMAIL_VERIFICATION = "mandatory"
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 
 # ---------------------------------------------------
-# MESSAGE STORAGE
+# MESSAGES
 # ---------------------------------------------------
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
