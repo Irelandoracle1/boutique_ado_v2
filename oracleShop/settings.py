580b7ec7a22b6b8c814659a5dfc6b68f6d1af43c
Oluwaseyi <nairajnr1@gmail.com>
2026-04-13 13:13:35 +0100

    fixing database issue

--- oracleShop/settings.py
+++ oracleShop/settings.py
@@ -169,11 +169,24 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # }
 
 
+
+
 DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL")
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+
+# ONLY use Postgres on Render
+if os.getenv("DATABASE_URL"):
+    import dj_database_url
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.getenv("DATABASE_URL"),
+            conn_max_age=600
+        )
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
